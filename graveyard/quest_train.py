73b7dd4bdc0c9877f44d034ff1fdf8af95243814
Katie <106988519+katie34634@users.noreply.github.com>
2026-05-06 22:35:11 -0500

    more latent space

--- graveyard/quest_train.py
+++
@@ -1,9 +0,0 @@
-import logging
-import graveyard.train as train
-
-logging.basicConfig(level=logging.INFO)
-
-if __name__ == "__main__":
-    logging.info("Calling train.main()")
-    train.main()
-    logging.info("Training finished")
